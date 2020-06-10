047d6be52faf35a9ecb3f19a34f86bcbc7f76cba
juan javier <jmarcosa@uni.pe>
2020-06-10 17:43:15 -0500

    cambios

--- app.js
+++ app.js
@@ -6,5 +6,16 @@ function validarDni(el) {
         el.value = el.value.substring(0, el.value.length - 1);
     }
     console.log(el.value.length)
+    if (el.value.length === 8) {
+        el.value = el.value.substring(0, 9)
+    }
+}
 
+function isNumber(evt) {
+    evt = (evt) ? evt : window.event;
+    var charCode = (evt.which) ? evt.which : evt.keyCode;
+    if (charCode > 31 && (charCode < 48 || charCode > 57)) {
+        return false;
+    }
+    return true;
 }
