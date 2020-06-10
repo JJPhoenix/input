1d90895d7f1480ca50e011fc79e4a4002346393a
juan javier <jmarcosa@uni.pe>
2020-06-10 17:50:36 -0500

    cambio3

--- app.js
+++ app.js
@@ -14,8 +14,9 @@ function validarDni(el) {
 function isNumber(evt) {
     evt = (evt) ? evt : window.event;
     var charCode = (evt.which) ? evt.which : evt.keyCode;
-    if (charCode > 31 && (charCode < 48 || charCode > 57)) {
-        return false;
+    console.log(charCode)
+    if (charCode > 48 && charCode < 57) {
+        return true;
     }
-    return true;
+    return false;
 }
