4742d466447a307004e999f3f84710a3a9cbb38e
juan javier <jmarcosa@uni.pe>
2020-06-10 17:25:09 -0500

    cambio

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const log = document.getElementById('log');
 
 function validarDni(el) {
     var ex = /^[0-9]+\.?[0-9]*$/;
-    if (ex.test(el.value) == false || el.value.length > 8) {
+    if (ex.test(el.value) == false) {
         el.value = el.value.substring(0, el.value.length - 1);
     }
     console.log(el.value.length)
