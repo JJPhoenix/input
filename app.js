bd9704ad2acf6643124092dc39fcdc35f8a7bac1
juan javier <jmarcosa@uni.pe>
2020-06-10 17:15:23 -0500

    cambios

---
+++ app.js
@@ -0,0 +1,12 @@
+const log = document.getElementById('log');
+
+function validarDni(el) {
+    var ex = /^[0-9]+\.?[0-9]*$/;
+    if (ex.test(el.value) == false || el.value.length > 8) {
+        el.value = el.value.substring(0, el.value.length - 1);
+    }
+    console.log(el.value.length)
+    if (el.value.length === 8) {
+        el.value = el.value.substring(0, 9)
+    } else if (el.value)
+}
